c45c0428a53f5fea2c3d8532ba08fa038baed67e
ImpulseAllen <44782629+ImpulseAllen@users.noreply.github.com>
2018-11-15 14:44:09 -0800

    week 13

--- settings.py
+++ settings.py
@@ -3,9 +3,23 @@ TITLE = "jumpy"
 WIDTH = 480
 HEIGHT = 600
 # frames per second
-FPS = 30
+FPS = 60
 # colors
 WHITE = (255, 255, 255)
 BLACK = (0,0,0)
 REDDISH = (240,55,66)
-RANDOM = (0,153,153)
+TEAL = (0,153,153)
+GREEN = (0,255,0)
+PINK = (255,0,127)
+FONT_NAME = 'arial'
+# player settings
+PLAYER_ACC = 0.5
+PLAYER_FRICTION = -0.12
+PLAYER_GRAV = 0.8
+PLAYER_JUMP = 20
+# platform settings
+PLATFORM_LIST = [(0, HEIGHT - 40, WIDTH, 40),
+                 (65, HEIGHT - 300, WIDTH-400, 40),
+                 (20, HEIGHT - 350, WIDTH-300, 40),
+                 (200, HEIGHT - 150, WIDTH-350, 40),
+                 (200, HEIGHT - 450, WIDTH-350, 40)]
