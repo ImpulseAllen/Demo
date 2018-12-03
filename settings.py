66aa0c769fd2c4b5a8f73234b90f5adada2e9c7f
ImpulseAllen <44782629+ImpulseAllen@users.noreply.github.com>
2018-12-03 09:55:28 -0800

    First

--- settings.py
+++ settings.py
@@ -8,18 +8,40 @@ FPS = 60
 WHITE = (255, 255, 255)
 BLACK = (0,0,0)
 REDDISH = (240,55,66)
-TEAL = (0,153,153)
-GREEN = (0,255,0)
-PINK = (255,0,127)
+SKY_BLUE = (143, 185, 252)
 FONT_NAME = 'arial'
+SPRITESHEET = "spritesheet_jumper.png"
+SPRITESHEET2 = "spritesheet.png"
+# data files
+HS_FILE = "highscore.txt"
 # player settings
 PLAYER_ACC = 0.5
 PLAYER_FRICTION = -0.12
 PLAYER_GRAV = 0.8
 PLAYER_JUMP = 20
+PLAYER_DUCK = 0.5
+# game settings
+BOOST_POWER = 60
+POW_SPAWN_PCT = 7
+MOB_FREQ = 500
+# layers - uses numerical value in layered sprites
+PLAYER_LAYER = 2
+PLATFORM_LAYER = 1
+POW_LAYER = 1
+MOB_LAYER = 2
+CLOUD_LAYER = 0
+
 # platform settings
+''' old platforms from drawing rectangles'''
+'''
 PLATFORM_LIST = [(0, HEIGHT - 40, WIDTH, 40),
                  (65, HEIGHT - 300, WIDTH-400, 40),
                  (20, HEIGHT - 350, WIDTH-300, 40),
                  (200, HEIGHT - 150, WIDTH-350, 40),
                  (200, HEIGHT - 450, WIDTH-350, 40)]
+'''
+PLATFORM_LIST = [(0, HEIGHT - 40),
+                 (65, HEIGHT - 300),
+                 (20, HEIGHT - 350),
+                 (200, HEIGHT - 150),
+                 (200, HEIGHT - 450)]
